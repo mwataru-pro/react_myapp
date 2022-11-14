1046fa7cbeca9061a625d7648c9d75648313e118
W.Mochizuki <m1130wata@gmail.com>
2022-11-14 16:22:01 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { ColoredMessage } from "./components/ColoredMessage";
+
+export const App = () => {
+  const onClickButton = () => {
+    alert();
+  }
+
+  const contentPinkStyle = {
+    color: "pink",
+    fontSize: "20px"
+  }
+
+  return (
+    <div>
+      {console.log("TEST")}
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <ColoredMessage/>
+      <p style={contentPinkStyle}>元気です!</p>
+      <button onClick={onClickButton}>ボタン</button>
+    </div>
+  );
+};
