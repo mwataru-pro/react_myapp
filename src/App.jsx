9bd6f47f4934d3c888a2db3edfe4c1283acb39e1
W.Mochizuki <m1130wata@gmail.com>
2022-11-15 19:06:59 +0900

    [add] childrenでメッセージを受け取る

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ export const App = () => {
     <div>
       {console.log("TEST")}
       <h1 style={{ color: "red" }}>こんにちは！</h1>
-      <ColoredMessage color="blue" message="お元気ですか？"/>
-      <ColoredMessage color="pink" message="元気です！"/>
+      <ColoredMessage color="blue">お元気ですか？</ColoredMessage>
+      <ColoredMessage color="pink">元気です！</ColoredMessage>
       <button onClick={onClickButton}>ボタン</button>
     </div>
   );
