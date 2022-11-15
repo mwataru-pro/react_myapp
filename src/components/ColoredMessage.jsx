162a4f123d4c5b4c0114be0376e5facfd008a2f5
W.Mochizuki <m1130wata@gmail.com>
2022-11-15 19:28:34 +0900

    [add] Propsの分割代入

--- src/components/ColoredMessage.jsx
+++ src/components/ColoredMessage.jsx
@@ -1,9 +1,11 @@
 export const ColoredMessage = (props) => {
 
+  const {color, children} = props;
+
   const contentStyle = {
-    color: props.color,
+    color,
     fontSize: "20px"
   };
 
-  return <p style={contentStyle}>{props.children}</p>
+  return <p style={contentStyle}>{children}</p>
 }
